8c0de611d68b77fb732a48b3ddfb51426b7ade8a
parisadikshit <2018bec034@sggs.ac.in>
2023-06-13 07:40:09 +0530

    Auth,user,bookmarks modules added

---
+++ bookmarks/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+
+@Controller('auth')
+export class AuthController{
+
+    constructor(private AuthService:AuthService){}
+
+    @Post('signup')
+    signup(){
+        return 'I am signup';
+    }
+    
+    @Post('signin')
+    signin(){
+        return 'I am signin';
+    }
+};
